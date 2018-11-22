79bea1269ccf427f0b34b41f15e001677b6b88eb
Radenko Kokanovic <radenko.kokanovic@gmail.com>
2018-11-22 22:29:15 +0100

    First commit

---
+++ BackendService/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+ 	@Id
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+ 	private String name;
+ 	private int age;
+ 	private int salary;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
+	}
+	public Employee(Integer id, String name, int age, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+ 	public Employee()
+ 	{
+ 		
+ 	}
+ 	
+
+}
---
+++ BackendService/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>{
+
+}
